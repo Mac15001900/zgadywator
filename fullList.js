4993b517e4a7bcb45db261f294847af690de80ef
Mac15001900 <mac15001900@gmail.com>
2023-10-06 13:28:08 +0200

    Turned word list to JavaScript

---
+++ fullList.js
@@ -0,0 +1,583 @@
+basicWords = `Richard Feynman
+Nicola Tesla
+Mikołaj Kopernik
+Erwin Schrödinger
+Max Planck
+Galileusz
+Eugeniusz Rybka
+Maria Skłodowska-Curie
+Mały Obłok Magellana
+Gromada otwarta w Herkulesie
+Katalog Messiera
+Gromada w Pannie
+Droga Mleczna
+Supergromada Lokalna
+Orion
+Ganimedes
+Fobos
+Wężownik
+Krater na Księżycu
+Karpaty
+Tarcza Sobieskiego
+Wielka Czerwona Plama
+Olympus Mons
+Galaktyka Wiatraczek
+Cefeusz
+Projekt OGLE
+Wybuch Betelgezy
+Układ podwójny
+Ciało doskonale czarne
+Widmo emisyjne
+Ciąg
+Jack Sparrow
+Gandalf Szary
+Fiona
+Mulan
+Igrzyska Śmierci
+Zwiadowcy
+Barbie
+Ken
+Oppenheimer
+Doda
+Michael Jackson
+Pałac Kultury
+Kubuś Puchatek
+Kłapouchy
+Adam Małysz
+Mariusz Pudzianowski
+Robert Lewandowski
+Usain Bolt
+Robert Kubica
+Elsa
+Roszpunka
+Tabaluga
+Papa Smerf
+Psi Patrol
+Gumisie
+Pepe Pan Dziobak
+Pan Tadeusz
+Koziołek Matołek
+Bolek I Lolek
+Jurand ze Spychowa
+Jagienka
+Calineczka
+Hermiona
+Severus Snape
+Voldemort
+Król Julian
+Obi Wan Kenobi
+Baby Yoda
+Kuce z Bronksu
+ACDC
+Uniwersytet Warszawski
+Władca Pierścieni
+Gollum
+Zgredek
+Iron Man
+Neil deGrasse Tyson
+Hulk
+Mario
+Neil Armstrong
+Jackie Chan
+Picasso
+Beethoven
+Rasputin
+Królowa Elżbieta
+Krzesło obrotowe
+Titanic
+Antarktyda
+Mur chiński
+Chińskie bajki
+Catering Werona
+Platon
+Konfucjusz
+Stoicyzm
+Poznań
+Aleje Jerozolimskie
+Kolumna Zygmunta
+Krzywa wieża w Pizie
+Toruńska starówka
+Smok wawelski
+Punkt Barana
+Montaż paralaktyczny
+Montaż azymutalny
+User
+Arbuz
+Ewakuacja
+Małe trąby powietrzne
+Kret
+Mariner
+Krople żołądkowe
+Hamak
+Gumofilce
+Wykryj porównaj
+Mikrofalówka komendanta
+Coś zielonego
+Ciepły asfalt
+Gąszcz
+Udar słoneczny
+Dżdż
+Kapuśniaczek
+Opad konwekcyjny
+dbz
+Arburza
+Kadra antywychowawcza
+G
+Brudne skarpetki
+Larwa
+Umieralnia
+Entropia
+Spin
+Dynks
+Tenteges
+Tętno
+Tkanka tłuszczowa
+Opornik
+Kosmiczne grzybki
+Kurz
+Układ nerwowy oposa
+Odpowiedzialność zbiorowa
+Motorniczy
+Gotyk
+Mikrosoczewkowanie grawitacyjne
+Grafen
+Mitoza
+Termopauza
+Lawa
+Pole elektromagnetyczne
+Bajt
+Tenor
+Trąba
+Proch strzelniczy
+Okrąg
+Sołtys
+Zbiór liczb całkowitych
+Nic
+Jajko
+Palambury
+Błękit
+Złość
+Juliusz Cezar
+Wielki Wybuch
+Pozytywizm
+Impresjonizm
+Rock psychodeliczny
+Starożytność
+Odkurzacz bezprzewodowy
+AstroNET
+Ekliptyka
+Rektascencja
+Jesteś dzikiem
+Wartowanie
+Linux
+Almukantywka
+Rabini
+Równowaga chwiejna
+Sortowanie bąbelkowe
+Uśmiech bąbelka
+Rozpuszczona czekolada
+Ekwiwalent kiełbasy
+Naleśnik z budyniem
+Gruszka
+Gofr z tofu
+Szpinak
+Eter
+Granit
+Kostka lodu
+Trójkąt
+Następna stacja
+Nieskończoność
+Politechnika Wrocławska
+3 kilogramy
+Rój pszczół
+Cynk
+Materiał wodoodporny
+Piorun
+Rów Mariański
+Próżnia
+Kciuk
+Zmarszczki na czole
+Powietrze
+Szum fal
+Światło
+Karteczka na plecach
+Ściółka na kanadyjce
+Apel
+Obozowa Olimpiada Astronomiczna
+Gwizdek oboźnej
+Czapka Sulika
+Komendant
+Altanka
+Serower
+Serowar
+Serakieta
+Wodąwryj
+Bardzo mała żaba
+Dinozaur Pimpuś
+Parówka sojowa
+Groźna oboźna
+Dziewczyna co piła gorące mleko
+Marszałkowie szklanoocy
+Sarna
+Kanadyjka
+Drechoteka
+Mokry latex
+Złota szyszka
+Plopserwacje
+Niebo żyleta
+Sen srebrny w płynie
+Sen srebrny w stanie stałym
+
+Klub Astrologiczny Akumulator
+Klub Gastronomiczny Almukantratat
+Pierwiastek 0
+Nadwarciański Grób
+Tosty z żelazka
+Zbiór pusty
+Granat zaczepny
+Drzewo sandałowe
+Pajęczy kosz
+Olej rzepakowy
+Redystrybucja butów
+Fregata uzbrojona rzędem w setkę dział
+Złota kula nad wodami
+Para bobrów przytulonych nad potokiem
+Ta keja
+Gwiazda Śmierci
+Człowiek który zgubił do domu swego drzwi
+Wiatr co rozwiewa stopy
+Piersi w panierce
+Przystojny wiatr
+Pieśń skrzydlata
+Zielone karty
+Jaworzyna
+Major Ponury
+Majster Bieda
+Posejdon
+Zeus
+Smutny programista
+Chomik na patelni
+Rybacki kuter
+Morska opowieść
+Całkiem nowa bomba atomowa
+Drzewo cytrynowe
+Diament
+Wehikuł czasu
+Minecraft
+Tetris
+Sudoku
+Kółko i Krzyżyk
+Kamień Papier i Nożyce
+Ktulu
+Zielona macka
+Hazardzista
+Tajniacy
+Pocisk
+Doktorat z Szyszkologii
+Petagram
+Pentagram
+Obrotowa mapka nieba
+Obrotowa mapka Dzietrznik
+Dzietrzniki
+Cisza nocna
+Czwarta nad ranem
+Woda gazowana
+Baniak z wodą
+Mech
+Litosfera
+Ośmiokąt nieforemny
+Kandydat Na Kandydata Na Weterana Obozów Astronomiczncyh
+Piąta ćwiartka
+Międzynarodowa Stacja Kosmiczna
+Tablica ogłoszeń
+Zbieranie szyszek
+Zeszyt w linie
+Matura
+Killer
+Czerwona latarka
+Wielka Całka
+Transformata Fouriera
+Sieć neuronowa
+ChatGPT
+Sztuczna inteligencja
+Sztuczna głupota
+Płyta DVD
+Dyskietka
+Packa na muchy
+Modliszka
+Pszczoła
+Bydło
+Spam
+Flaga Finlandii
+Kraken
+Trebusz
+Mikser
+Obieraczka
+Pikantny sos
+Niezapominajka
+Internet
+Pianka do golenia
+Terraformacja
+Wojna nuklearna
+Hoplita
+Rów Mariański
+Gripex
+Firanka
+Szuflada
+Snopowiązałka
+Zagadka
+Armagedon
+Powódź
+Śrubokręt
+Para wodna
+Rewolucja przemysłowa
+Szczotka do włosów
+Żarówka
+Aorta
+Klawiatura
+Karta graficzna
+Papier ścierny
+Telefon wielokomórkowy
+Wirtualna rzeczywistość
+Święty Mikołaj
+Skórka od banana
+Kalkulator graficzny
+Szwajcarski zegarek
+Rondo
+Rondel
+Karabin snajperski
+
+Almukantarat
+Heliomagnetohydrodynamika
+Śpiewanki
+Gitara
+Załęcze
+Szyszka
+Harcerz
+Latający namiot
+Rozpórka
+Kontrola czystości
+Sanitariat
+Młyn
+Podwieczorek
+Zbiórka
+Pociąg elektryczny
+Kadron
+Wielki Zderzacz Kadronów
+Gra terenowa
+Ognisko
+Obóz
+Śpiwór
+Seminarium
+Zimowisko
+Referat
+Zwycięzca sesji referatowej
+Pojemnik ewakuacyjny
+Skrót myślowy
+
+Śląsk
+Bieszczady
+Paryż
+Teksas
+Hawaje
+Japonia
+Sahara
+Tybet
+Tatry
+Mount Everest
+Antarktyka
+Amazonka
+Google
+Telewizja
+GPS
+Satelita
+Pizza z ananasem
+UFO
+Kremówka
+Bursztyn
+Dzban
+
+Wielbłąd
+Żyrafa
+Krokodyl
+Orzeł
+Wieloryb
+Zółw
+Mamut
+Koliber
+Biedronka
+
+Trawa
+Sekwoja
+Melon
+Jabłonka
+Palma
+
+Przeciwieństwo
+Synonim
+Czas przeszły
+Liczba mnoga
+Łacina
+Polonistyka
+Japonistyka
+Biologia
+Filozofia
+Laboratorium matematyczne
+
+Magnetyzm
+Elektryczność
+Grawitacja
+Energia
+Przewodnik
+Nadprzewodnik
+Opór
+Wymiar
+Pęd
+Układ odniesienia
+Kwark
+Atom
+Superpozycja
+Splątanie
+Niepewność
+Fala
+Promieniowanie
+Pryzmat
+Radar
+Soczewka
+Laser
+Wahadło
+Sprężyna
+Asteroida
+Kometa
+Planeta X
+Księżyc
+Zaćmienie Słońca
+Plama słoneczna
+Koronalny wyrzut masy
+Gwiazda neutronowa
+Czarna dziura
+Tunel czasoprzestrzenny
+Galaktyka
+Wszechświat
+Ciemna materia
+Jasna materia
+Wenus
+Mars
+Pas asteroid
+Perseidy
+Jowisz
+Saturn
+Uran
+Pluton
+Charon
+Obłok Oorta
+Teleskop radiowy
+Winda kosmiczna
+Kot Schrödingera
+
+Magia
+Zaklęcie
+Alchemia
+Mandragora
+Runa
+Iluzja
+Pułapka
+Krasnolud
+Elf
+Syrena
+Driada
+Centaur
+Ork
+Troll
+Skrzat
+Demon
+Smok
+Gryf
+Duch
+Zombie
+Szkielet
+Jednorożec
+Minotaur
+Goblin
+Różdżka
+Latający dywan
+Peleryna niewidka
+Eliksir
+Magiczna kula
+Kocioł
+Czarodziej
+Wiedźma
+Barbarzyńca
+Kleryk
+Złodziej`
+
+
+harderWords = `Kondensat Bosego-Einsteina
+Enrico Fermi
+Krzysztof Meissner
+Subrahmanyan Chandrasekhar
+Niels Bohr
+Paul Dirac
+Alexander Fleming
+Stefan Banach
+Władysław Dziewulski
+Mirosław Hermaszewski
+Sławosz Uznański
+Prawo Wiena
+Henry Cavill
+Keanu Reeves
+Olga Tokarczuk
+Tadeusz Sznuk
+Robert Makłowicz
+Margot Robbie
+Harry Styles
+Kylian Mbape
+Olivia Rodrigo
+Billie Eilish
+Dua Lipa
+Ed Sheeran
+Sanah
+Taco Hemingway
+Miley Cyrus
+Izabela Łęcka
+Cezik
+Krzysztof Ibisz
+Dawid Podsiadło
+Igor Kamiński
+Kylie Jenner
+Ryan Gossling
+Iga Świątek
+Katherine Johnson
+Henrietta Leavit
+Will Smith
+Brad Pitt
+Frida Kahlo
+Yennefer
+Maria Antonina
+Gyrochronologia
+Waltornia
+Lira korbowa
+Dąb “Bartek”
+Tutanchamon
+Czyngis-chan
+Stanisław Wyspiański
+Banksy
+Michał Wójcik
+Leonardo DiCaprio
+Angelina Jolie
+BajoJarek
+Piotr Łubis
+Idefix
+Kapłanki rozkoszy
+Ojciec Mateusz
+Morgan Freeman
+Czyngis-chan
+Imagine Dragons
+Rammstein
+Taylor Swift
+Mikoryza
+Iwan Groźny
+Zaświadczenie A39
+Pani Beatka
+Janosik
+WD-40`
